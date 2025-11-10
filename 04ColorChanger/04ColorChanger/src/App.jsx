70b032db3e50c811180107844e124258830fe002
Shivangi <81459301+shivangi-verma@users.noreply.github.com>
2025-11-10 23:21:58 +0530

    update

--- 04ColorChanger/04ColorChanger/src/App.jsx
+++ 04ColorChanger/04ColorChanger/src/App.jsx
@@ -56,39 +56,6 @@ function App() {
           </button>
         </div>
       </div>
-      {/* <div
-        className=" h-screen w-screen m-auto  "
-        style={{ backgroundColor: color }}
-      >
-        <h1 className="text-xl font-bold m-auto ">color changer</h1>
-        <div className=" fixed flex flex-wrap justify-center m-5">
-          <button
-            type="button"
-            className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 border border-white"
-            onClick={() => setColor("green")}
-          >
-            Green
-          </button>
-          <button
-            type="button"
-            className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-          >
-            Red
-          </button>
-          <button
-            type="button"
-            className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
-          >
-            Yellow
-          </button>
-          <button
-            type="button"
-            className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-          >
-            Purple
-          </button>
-        </div>
-      </div> */}
     </>
   );
 }
