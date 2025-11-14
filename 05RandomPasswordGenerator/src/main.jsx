89ddbb30fa6456ab930cbe2a7e5e52330bcf05c9
Shivangi <81459301+shivangi-verma@users.noreply.github.com>
2025-11-14 15:33:32 +0530

    add password generator component and update dependencies

--- 05RandomPasswordGenerator/src/main.jsx
+++ 05RandomPasswordGenerator/src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import PassGen from "./components/PassGen.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <PassGen/>
   </StrictMode>
 );
