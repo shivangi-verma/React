d38a3d34394a7cef512cb8857f8f46acace5c6b3
Shivangi <81459301+shivangi-verma@users.noreply.github.com>
2025-11-21 18:02:51 +0530

    fix Clock component to correctly update time using setInterval and state

--- first_project/src/components/Clock.jsx
+++ first_project/src/components/Clock.jsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react";
 
 function Clock() {
-  const [timeEl, setTimeEl] = useState(new Date().toLocaleTimeString());
-  
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
   useEffect(() => {
-    const setInterval = setTimeout(setTimeEl, 1000);
-    setTimeEl(setInterval);
-    return () => clearTimeout(setInterval);
-  }, [timeEl]);
+    const intervalId = setInterval(() => {
+      setTime(new Date.toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <div>
       <h1>Clock</h1>
-      <h2>Time : {timeEl}</h2>
+      <h2>Time : {time}</h2>
     </div>
   );
 }
